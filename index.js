7bf50aeec2e69a6d7ee59e3b1074487cd75b4017
Avelina Kim <avelina@gmail.com>
2019-04-24 18:52:45 -0700

    create findRoute function

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-///////////////////////////////////////////////////////////////////
 // Build Roads
 
 const roads = [
@@ -134,3 +133,21 @@ function routeRobot(state, memory) {
 }
 
 runRobot(VillageState.random(), routeRobot, []);
+
+///////////////////////////////////////////////////////////////////
+// Find Route function
+
+function findRoute(graph, from, to) {
+  let work = [{ at: from, route: [] }];
+  for (let i = 0; i < work.length; i++) {
+    let { at, route } = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some(w => w.at == place)) {
+        work.push({ at: place, route: route.concat(place) });
+      }
+    }
+  }
+}
+
+
